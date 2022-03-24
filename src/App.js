6f40c21579cb2ee4a972f6e2136f16a519e5cee8
Brandon Vergara <brandon.vergara@kueski.com>
2022-03-24 01:02:25 -0600

    [master] add logout to page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
 import Profile from './components/Profile';
 
 function App() {
   return (
     <div className="App">
+      <div className="header-text">
+        Example login with Auth0
+      </div>
       <LoginButton />
+      <div style={{marginBottom: '90px'}} />
       <Profile />
+      <div style={{marginBottom: '90px'}} />
+      <LogoutButton />
     </div>
   );
 }
